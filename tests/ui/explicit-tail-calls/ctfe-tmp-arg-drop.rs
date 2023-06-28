b902f1fb68d2e515a6cf5949539c8edee0481428
Maybe Waffle <waffle.lapkin@gmail.com>
2023-06-28 15:34:10 +0000

    Support tail calls in the interpreter

---
+++ tests/ui/explicit-tail-calls/ctfe-tmp-arg-drop.rs
@@ -0,0 +1,24 @@
+#![allow(incomplete_features)]
+#![feature(explicit_tail_calls, const_trait_impl, const_mut_refs)]
+
+pub const fn test(_: &View) {
+    const fn takes_view(_: &View) {}
+
+    become takes_view(HasDrop.as_view());
+    //~^ error: temporary value dropped while borrowed
+}
+
+struct HasDrop;
+struct View;
+
+impl HasDrop {
+    const fn as_view(&self) -> &View {
+        &View
+    }
+}
+
+impl const Drop for HasDrop {
+    fn drop(&mut self) {}
+}
+
+fn main() {}
